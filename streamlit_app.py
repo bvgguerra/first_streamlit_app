fd51e35b4eb537089ea2cad34ee015338ffd1b9b
bvgguerra <124951604+bvgguerra@users.noreply.github.com>
2023-02-10 17:44:29 +0800

    Update streamlit_app.py
    added ability to look up fruit with fruityvice api

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+#allow user to look up fruit
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #normalize json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output as table
