5fec682844ca9aacaa0bfc8491a8aabb98a1eed6
bvgguerra <124951604+bvgguerra@users.noreply.github.com>
2023-02-10 17:45:45 +0800

    Update streamlit_app.py
    edited request to actually load up the entered fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -27,6 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 #allow user to look up fruit
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 #normalize json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
